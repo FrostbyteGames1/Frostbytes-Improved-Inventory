9281efc55fbf669d5dba851944189e1c563bd84e
Srithan Surapaneni <109047591+FrostbyteGames1@users.noreply.github.com>
2023-12-15 14:55:55 -0500

    Fixed stack refill

--- src/main/java/net/frostbyte/inventory/StackRefiller.java
+++ src/main/java/net/frostbyte/inventory/StackRefiller.java
@@ -6,7 +6,9 @@ import com.google.gson.JsonObject;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.screen.slot.SlotActionType;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -17,14 +19,14 @@ public class StackRefiller implements ClientTickEvents.EndTick {
     final Gson gson = new GsonBuilder().setPrettyPrinting().create();
     public static boolean stackRefill = true;
     MinecraftClient mc;
+    Item item = Items.AIR;
+    int slot = -1;
 
     @Override
     public void onEndTick(MinecraftClient client) {
-        ClientPlayerEntity player;
         mc = client;
-        player = client.player;
 
-        if (player == null) {
+        if (mc.player == null) {
             return;
         }
 
@@ -37,6 +39,43 @@ public class StackRefiller implements ClientTickEvents.EndTick {
                 stackRefill = json.getAsJsonPrimitive("stackRefill").getAsBoolean();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+        if (stackRefill) {
+            tryRefillStack();
+        }
+    }
+
+    void tryRefillStack() {
+        assert mc.player != null;
+        if (mc.player.currentScreenHandler.getStacks().size() == 46 && mc.currentScreen == null) {
+            if (mc.player.getInventory().getMainHandStack().isEmpty() && item != Items.AIR && slot == mc.player.getInventory().selectedSlot) {
+                for (int i = 35; i > 8; i--) {
+                    if (mc.player.getInventory().getStack(i).getItem() == item) {
+                        assert mc.interactionManager != null;
+                        mc.interactionManager.clickSlot(mc.player.playerScreenHandler.syncId, i, mc.player.getInventory().selectedSlot, SlotActionType.SWAP, mc.player.getInventory().player);
+                        mc.player.getInventory().markDirty();
+                        mc.player.playerScreenHandler.sendContentUpdates();
+                        mc.player.playerScreenHandler.updateToClient();
+                        return;
+                    }
+                    if (item.isFood() && item != Items.GOLDEN_APPLE && item != Items.ENCHANTED_GOLDEN_APPLE && item != Items.POPPED_CHORUS_FRUIT) {
+                        if (mc.player.getInventory().getStack(i).getItem().isFood() && mc.player.getInventory().getStack(i).getItem() != Items.GOLDEN_APPLE && mc.player.getInventory().getStack(i).getItem() != Items.ENCHANTED_GOLDEN_APPLE && mc.player.getInventory().getStack(i).getItem() != Items.POPPED_CHORUS_FRUIT) {
+                            assert mc.interactionManager != null;
+                            mc.interactionManager.clickSlot(mc.player.playerScreenHandler.syncId, i, mc.player.getInventory().selectedSlot, SlotActionType.SWAP, mc.player.getInventory().player);
+                            mc.player.getInventory().markDirty();
+                            mc.player.playerScreenHandler.sendContentUpdates();
+                            mc.player.playerScreenHandler.updateToClient();
+                            return;
+                        }
+                    }
+                }
+            }
+            item = mc.player.getInventory().getMainHandStack().getItem();
+            slot = mc.player.getInventory().selectedSlot;
+        } else {
+            item = Items.AIR;
+            slot = -1;
         }
     }
 }
